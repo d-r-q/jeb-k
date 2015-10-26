f80981784dc95025c566a24cc66cb5e3ddf64c1c
Aleksey Zhidkov <aleksey.zhidkov@gmail.com>
2015-10-26 19:59:22 +0600

    Cleanup hanoi

--- src/main/kotlin/jeb/hanoi.kt
+++ src/main/kotlin/jeb/hanoi.kt
@@ -4,28 +4,18 @@ import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import java.util.*
 
-operator fun List<Int>.compareTo(another: List<Int>) = when {
-    this.isEmpty() && another.isEmpty() -> 0
-    this.isEmpty() -> 1
-    another.isEmpty() -> -1
-    else -> this.last() - another.last()
-}
-
-fun <T> List<T>.moveTo(another: List<T>): Pair<List<T>, List<T>> = Pair(
-        this.subList(0, this.size - 1),
-        ArrayList(another) + (this as Iterable<T>).last())
-
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Hanoi @JsonCreator constructor (
         val pegs: List<List<Int>>,
         val step: Int) {
-    private val descComparator = Comparator { a: Int, b: Int -> b - a }
 
-    private val disks = pegs.fold(sortedSetOf<Int>(descComparator)) { acc, e -> acc.addAll(e); acc }
+    private val diskComparator = Comparator { a: Int, b: Int -> b - a }
+    private val disks = pegs.fold(sortedSetOf<Int>(diskComparator)) { acc, e -> acc.addAll(e); acc }
+
     private val oddMoves = listOf(0 to 2, 0 to 1, 1 to 2)
-
     private val evenMoves = listOf(0 to 1, 0 to 2, 1 to 2)
     private val moves = if (disks.size % 2 == 0) evenMoves else oddMoves
+
     val done = pegs[0].size == 0 && pegs[1].size == 0
 
     val largestDisc = disks.first()
@@ -55,3 +45,14 @@ data class Hanoi @JsonCreator constructor (
     operator fun get(pegIdx: Int) = pegs[pegIdx]
 
 }
+
+operator fun List<Int>.compareTo(another: List<Int>) = when {
+    this.isEmpty() && another.isEmpty() -> 0
+    this.isEmpty() -> 1
+    another.isEmpty() -> -1
+    else -> this.last() - another.last()
+}
+
+fun <T> List<T>.moveTo(another: List<T>): Pair<List<T>, List<T>> = Pair(
+        this.subList(0, this.size - 1),
+        ArrayList(another) + this[this.lastIndex])
