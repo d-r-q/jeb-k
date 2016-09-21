cc3ba71acd6c1b301b78904b3f6bc32bfda9cd6c
Aleksey Zhidkov <aleksey.zhidkov@gmail.com>
2016-09-21 19:19:35 +0700

    Allow user to input multiple sources via cli Closes #3

--- src/main/kotlin/jeb/jeb.kt
+++ src/main/kotlin/jeb/jeb.kt
@@ -4,6 +4,7 @@ import jeb.util.Try
 import java.io.BufferedReader
 import java.io.File
 import java.time.LocalDateTime
+import java.util.*
 import javax.swing.JOptionPane
 
 val usage = "Usage: jeb-k <init|backup <dir>>"
@@ -19,17 +20,17 @@ fun main(args: Array<String>) {
 
 fun init() {
     val currentDir = System.getProperty("user.dir").withTailingSlash()
-    val sourceDir = readLine("Source directory", currentDir)
+    val sourceDirs = readSourceDirs(currentDir)
 
-    val backupDirDefault = if (File(currentDir) != File(sourceDir)) currentDir else null
+    val backupDirDefault = if (sourceDirs.contains(currentDir)) null else currentDir
     val backupDir = readLine("Backups directory", backupDirDefault)
     val disksCount = readLine("Backups count", "10").toInt()
 
-    val state = State(backupDir, listOf(Source(sourceDir)), Hanoi(disksCount))
+    val state = State(backupDir, sourceDirs.map(::Source), Hanoi(disksCount))
     State.saveState(File(backupDir, "jeb.json"), state)
 }
 
-fun backup(backupDir: File, time: LocalDateTime) {
+private fun backup(backupDir: File, time: LocalDateTime) {
     val config = File(backupDir, "jeb.json")
     if (!config.exists()) {
         println("jeb-k config is not found at ${config.absolutePath}")
@@ -50,6 +51,21 @@ private fun doBackup(config: File, state: State, time: LocalDateTime) {
     } catch(e: JebExecException) {
         JOptionPane.showMessageDialog(null, e.toString(), "title", JOptionPane.ERROR_MESSAGE)
     }
+}
+
+private fun readSourceDirs(currentDir: String): List<String> {
+    val sources = arrayListOf<String>()
+    var defaultSourceDir: String? = currentDir
+    do {
+        val invitation = if (sources.size == 0) "Source directory (add trailing slash to backup directory content or leave it blank to backup directory)"
+        else "Source directory"
+        val source = readLine(invitation, defaultSourceDir)
+        if (source == defaultSourceDir) {
+            defaultSourceDir = null
+        }
+        sources.add(source)
+    } while (readLine("Add another source? [yes/No]", "no").toLowerCase() == "yes")
+    return sources
 }
 
 // Hack for tests: do not recreate buffered reader on each call
